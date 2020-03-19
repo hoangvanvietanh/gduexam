6d80bd5606a9606b1ff751a72916b8b349d25030
Hoàng Văn Việt Anh <37948602+hoangvanvietanh@users.noreply.github.com>
2020-03-19 16:23:24 +0700

    ok

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports={
     "clientID"      :     "1121230534890039", //Điền App ID của bạn vào đây
     "clientSecret"   :     "74f9f24ff65d3ce868bdb67089ae76b6", //Điền App Secret ở đây
-    "callbackURL"      :     "https://gduexam.hoangvanvietanh.com/auth/facebook/callback"
+    "callbackURL"      :     "https://daihocgiadinh.com.vn/auth/facebook/callback"
   }
